20af199bcc4b82add57c8e4b551043e55877782a
Semenovdev <semenovdev@yahoo.com>
2022-06-14 13:34:42 +0600

    add colors

--- src/lib/colors/colors.ts
+++ src/lib/colors/colors.ts
@@ -1,5 +1,13 @@
 export const COLORS = {
-  white: '#fff',
-  black: '#000',
-  red: '#e63946'
+  white: '#FFFFFF',
+  orange: '#FF9E2C',
+  green: '#05C168',
+  lightRed: '#FC5842',
+  red: '#FF5A65',
+  darkRed: '#DC2B2B',
+  lightGray: '#C7C7C7',
+  gray: '#969696',
+  darkGray: '#393939',
+  darkestGray: '#272727',
+  black: '#181818',
 }
