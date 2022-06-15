d9ddc7662bac34d085cae8931d149b7be53a8a74
Semenovdev <semenovdev@yahoo.com>
2022-06-15 09:41:44 +0600

    fonts

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,11 +4,15 @@ import styled from "styled-components"
 export const Footer: React.FC = () => {
   return(
     <>
+      <Decription>Ut enim ad minim veniam quis<br/>nostrud exercitation  ea commodo</Decription>
       <Title>Footer</Title>
     </>
   )
 }
 
 const Title = styled.h3`
-  color: ${COLORS.white}
+  color: ${COLORS.white};`
+
+const Decription = styled.p`
+
 `
