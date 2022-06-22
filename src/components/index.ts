981ab40de16fcc67bc86cedcbfa8a3ae33f5d9ee
Semenovdev <semenovdev@yahoo.com>
2022-06-22 15:18:47 +0600

    components refactoring

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,3 @@
 export * from './main-layout'
-export * from './main-layout/footer/social-media'
-export * from './card'
 export * from './common' 
 export * from './pages'
--- src/components/main-layout/main-layout.tsx
+++ src/components/main-layout/main-layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { COLORS } from "src/lib";
 import { Header, Footer } from "src/components";
 
 interface IMainLayout {
