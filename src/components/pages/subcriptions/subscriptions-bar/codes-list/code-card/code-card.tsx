2c72cfc36e1bccbfa9ec2221100d8b5223f44d77
Semenovdev <semenovdev@yahoo.com>
2022-06-30 11:47:16 +0600

    Codes page ready

--- src/components/pages/subcriptions/subscriptions-bar/codes-list/code-card/code-card.tsx
+++ src/components/pages/subcriptions/subscriptions-bar/codes-list/code-card/code-card.tsx
@@ -2,7 +2,7 @@ import { ICode } from "src/types";
 import styled from "styled-components";
 import { SubscriptionP, SubscriptionH3 } from "src/components";
 import { CopyIcon } from "src/assets/icons";
-import { Checkbox } from "src/ui";
+import { Checkbox, Button } from "src/ui";
 import { useState } from "react";
 
 interface ICodesCard {
@@ -20,25 +20,28 @@ export const CodeCard: React.FC<ICodesCard> = ({ code }) => {
       <SCheckBlock>
         <Checkbox isChecked={isChecked} handleChange={handleChange} />
       </SCheckBlock>
-      <SLicense>
+      <div>
         <SubscriptionP>License code</SubscriptionP>
         <SLicenseCode>
           <SubscriptionP>{code.code}</SubscriptionP>
           <CopyIcon />
         </SLicenseCode>
-      </SLicense>
-      {/* <SLicense>
-            <SubscriptionP>Domain</SubscriptionP>
-            <SLicenseCode>
-              {code.origin}
-            </SLicenseCode>s
-          </SLicense>
-          <SLicense>
-            <SubscriptionP>Status</SubscriptionP>
-            <SLicenseCode>
-              {code.origin}
-            </SLicenseCode>
-          </SLicense> */}
+      </div>
+      <SDomain>
+        <SubscriptionP>Domain</SubscriptionP>
+        <SDomainBlock>
+          <SLink>{code.origin}</SLink>
+          {code.status === "INACTIVE" && (
+            <Button theme="secondary">Activate</Button>
+          )}
+        </SDomainBlock>
+      </SDomain>
+      <SStatus status={code.status}>
+        <SubscriptionP>Status</SubscriptionP>
+        <SubscriptionH3>
+          {code.status.charAt(0) + code.status.slice(1).toLowerCase()}
+        </SubscriptionH3>
+      </SStatus>
     </SCodesItem>
   );
 };
@@ -53,12 +56,10 @@ const SCodesItem = styled.li`
   background-color: var(--darkest-gray);
 `;
 
-const SLicense = styled.div``;
-
 const SLicenseCode = styled.div`
   max-width: 296px;
   margin-top: 12px;
-  padding: 25px 12px 25px 24px;
+  padding: 18px 12px 18px 24px;
   border-radius: 12px;
   display: flex;
   flex-direction: row;
@@ -74,5 +75,46 @@ const SLicenseCode = styled.div`
 `;
 
 const SCheckBlock = styled.div`
-  margin: 20px 48px 0 0 ;
+  margin: 20px 48px 0 0;
+`;
+
+const SDomain = styled.div`
+  flex: 1 1 0px;
+  margin: 0 56px 0 28px;
+`;
+
+const SDomainBlock = styled.div`
+  height: 68px;
+  margin-top: 12px;
+  display: flex;
+  flex-direction: row;
+  gap: 59px;
+`;
+
+const SLink = styled.div`
+  height: 68px;
+  padding: 25px 12px 25px 24px;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex: 1 1 0;
+  background-color: var(--dark-gray);
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;
+
+const SStatus = styled.div<{ status: string }>`
+  height: 98px;
+  align-self: flex-start;
+  h3 {
+    padding: 32px 0 20px;
+    color: ${(props) =>
+      props.status === "ACTIVE"
+        ? "var(--green)"
+        : props.status === "INACTIVE"
+        ? "var(--red)"
+        : "var(--orange)"};
+  }
 `;
