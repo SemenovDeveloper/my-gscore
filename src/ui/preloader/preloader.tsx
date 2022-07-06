37e23dee892c6e22ef1d1df6b9a065976b28ffaf
Semenovdev <semenovdev@yahoo.com>
2022-07-06 10:13:28 +0600

    preloader fixes

--- src/ui/preloader/preloader.tsx
+++ src/ui/preloader/preloader.tsx
@@ -1,9 +1,18 @@
+import styled from "styled-components"
 import { ContentContainer } from "../content-container"
 
 export const Preloader: React.FC =() => {
   return (
-    <ContentContainer>
-      <h1>Loading</h1>
-    </ContentContainer>
+    <Root>
+      <h1>Loading...</h1>
+    </Root>
   )
 }
+
+const Root = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+`
